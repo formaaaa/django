e86bdb83cf6e38ae657e7fdb938616da2d611e8a
formaaaa <83125660+formaaaa@users.noreply.github.com>
2021-07-27 21:10:08 +0200

    #commit 2

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -3,3 +3,11 @@ from django.db import models
 
 class Poll(models.Model):
     name = models.CharField(max_length=128)
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=128)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE, null=True, blank=True)
+
+
