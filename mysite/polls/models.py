295ff6ce317020d8b50c6533db4b8ff266f17b0d
formaaaa <83125660+formaaaa@users.noreply.github.com>
2021-07-28 19:29:59 +0200

    #commit 3

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -8,6 +8,6 @@ class Poll(models.Model):
 class Question(models.Model):
     question_text = models.CharField(max_length=128)
     pub_date = models.DateTimeField(auto_now_add=True)
-    poll = models.ForeignKey(Poll, on_delete=models.CASCADE, null=True, blank=True)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE, null=True, blank=True, related_name="question")
 
 
